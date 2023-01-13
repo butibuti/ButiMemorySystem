8f76071f25afa25b5b0ac35e3b373b6cbcb7d1ca
butibuti <butikouki@gmail.com>
2023-01-13 14:12:02 +0900

    メモリ確保変更

--- ButiMemorySystem/MemoryAllocator.cpp
+++ ButiMemorySystem/MemoryAllocator.cpp
@@ -2,36 +2,78 @@
 #include<mutex>
 #include"MemoryAllocator.h"
 #include<iostream>
+#include<unordered_map>
+#include<cassert>
 std::mutex mtx_alloc;
+
+constexpr std::uint64_t fieldSize = 0x4fffffff;
+
+struct Field {
+	Field() {
+		m_memory = reinterpret_cast<char*>(malloc(fieldSize));
+	}
+	~Field() {
+		free(m_memory);
+	}
+	char* m_memory;
+	std::uint64_t m_index=0;
+	std::unordered_map<void*, std::uint32_t> m_map_use_ptr;
+	std::unordered_map<std::uint32_t,std::vector<void*>> m_map_release_ptr;
+	void* GetPtr(const std::uint32_t arg_size)
+	{
+
+		if (!m_map_release_ptr.count(arg_size)) {
+			m_map_release_ptr.emplace(arg_size, std::vector<void*>());
+		}else
+		if (m_map_release_ptr.at(arg_size).size()) {
+			auto output = *m_map_release_ptr.at(arg_size).rbegin();
+			m_map_release_ptr.at(arg_size).pop_back();
+			m_map_use_ptr.emplace(output, arg_size);
+			return output;
+		}
+		assert(m_index < fieldSize);
+		m_map_use_ptr.emplace(&m_memory[m_index], arg_size);
+		auto output = &m_memory[m_index];
+		m_index += arg_size;
+		return output;
+	}
+	void Release(void* arg_ptr) {
+		m_map_release_ptr.at(m_map_use_ptr.at(arg_ptr)).push_back(arg_ptr);
+		m_map_use_ptr.erase(arg_ptr);
+
+	}
+};
+
+Field g_field;
 
 void* ButiMemorySystem::Allocator::allocate(std::uint32_t arg_size)
 {
 	std::lock_guard lock(mtx_alloc);
-	return (operator new(arg_size));
+	return g_field.GetPtr((arg_size + 0xf) & ~0xf);
 }
 
 void* ButiMemorySystem::Allocator::allocateLarge(std::uint64_t arg_size)
 {
 	std::lock_guard lock(mtx_alloc);
-	return (operator new(arg_size));
+	return g_field.GetPtr((arg_size + 0xf) & ~0xf);
 }
 
 void* ButiMemorySystem::Allocator::allocateArray(std::uint32_t arg_size)
 {
 	std::lock_guard lock(mtx_alloc);
-	return	operator new[](arg_size);
+	return	g_field.GetPtr((arg_size + 0xf) & ~0xf);
 }
 
 void* ButiMemorySystem::Allocator::allocateCustomAlign(std::uint64_t arg_size,std::int32_t alignment)
 {
 	std::lock_guard lock(mtx_alloc);
-	return (operator new(arg_size));
+	return g_field.GetPtr((arg_size + 0xf) & ~0xf);
 }
 
 void ButiMemorySystem::Allocator::deallocate(void* arg_ptr)
 {
 	std::lock_guard lock(mtx_alloc);
-	operator delete(arg_ptr);
+	g_field.Release(arg_ptr);
 }
 
 void ButiMemorySystem::Allocator::deallocateBt(void* arg_ptr)
@@ -42,5 +84,5 @@ void ButiMemorySystem::Allocator::deallocateBt(void* arg_ptr)
 void ButiMemorySystem::Allocator::deallocateArray(void* arg_ptr)
 {
 	std::lock_guard lock(mtx_alloc);
-	operator delete[](arg_ptr);
+	g_field.Release(arg_ptr);
 }
