c61e2e3a3626abbf2960b2f6df496ebda000c401
butibuti <butikouki@gmail.com>
2023-01-14 20:35:09 +0900

    確保変更

--- ButiMemorySystem/MemoryAllocator.cpp
+++ ButiMemorySystem/MemoryAllocator.cpp
@@ -11,6 +11,7 @@ constexpr std::uint64_t fieldSize = 0x4fffffff;
 struct Field {
 	Field() {
 		m_memory = reinterpret_cast<char*>(malloc(fieldSize));
+		m_vec_releasePtr.resize(2048);
 	}
 	~Field() {
 		free(m_memory);
@@ -18,27 +19,29 @@ struct Field {
 	char* m_memory;
 	std::uint64_t m_index=0;
 	std::unordered_map<void*, std::uint32_t> m_map_use_ptr;
-	std::unordered_map<std::uint32_t,std::vector<void*>> m_map_release_ptr;
+
+	std::vector<std::vector<void*>>m_vec_releasePtr;
 	void* GetPtr(const std::uint32_t arg_size)
 	{
-
-		if (!m_map_release_ptr.count(arg_size)) {
-			m_map_release_ptr.emplace(arg_size, std::vector<void*>());
-		}else
-		if (m_map_release_ptr.at(arg_size).size()) {
-			auto output = *m_map_release_ptr.at(arg_size).rbegin();
-			m_map_release_ptr.at(arg_size).pop_back();
-			m_map_use_ptr.emplace(output, arg_size);
+		std::int32_t unit = arg_size / 0xf;
+		if (m_vec_releasePtr.size()<(unit+1)) {
+			m_vec_releasePtr.resize(unit+1);
+		}
+		
+		if (m_vec_releasePtr[unit].size()) {
+				auto output = *m_vec_releasePtr[unit].rbegin();
+				m_vec_releasePtr[unit].pop_back();
+			m_map_use_ptr.emplace(output, unit);
 			return output;
 		}
 		assert(m_index < fieldSize);
-		m_map_use_ptr.emplace(&m_memory[m_index], arg_size);
+		m_map_use_ptr.emplace(&m_memory[m_index], unit);
 		auto output = &m_memory[m_index];
 		m_index += arg_size;
 		return output;
 	}
 	void Release(void* arg_ptr) {
-		m_map_release_ptr.at(m_map_use_ptr.at(arg_ptr)).push_back(arg_ptr);
+		m_vec_releasePtr[m_map_use_ptr.at(arg_ptr)].push_back(arg_ptr);
 		m_map_use_ptr.erase(arg_ptr);
 
 	}
